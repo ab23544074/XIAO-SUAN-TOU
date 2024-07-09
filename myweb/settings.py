038f23dd12f26f37e53776ea80aa08106c22cac0
Selena <ab23544074@gmail.com>
2024-07-09 17:21:23 +0800

    0709-7

--- myweb/settings.py
+++ myweb/settings.py
@@ -96,12 +96,13 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.mysql',
         'NAME': 'HuangFamily_store', #database name
-        'USER': 'Selena',
-        'PASSWORD': 'ab23544074',
-        'HOST': '192.168.56.1',
-        'PORT': '3309',
+        #'USER': 'Selena',
+        #'PASSWORD': 'ab23544074',
+        #'HOST': '192.168.56.1',
+        #'PORT': '3309',
     }
 }
 
