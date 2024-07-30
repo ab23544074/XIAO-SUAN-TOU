8921c7fb181a341631e8ecd020631bdb8767dd6d
selena <ab23544074@gmail.com>
2024-07-30 11:56:50 +0800

    20240730-3

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -47,7 +47,7 @@
             }
         }
         try {
-                data = await fetch('https://xiao-suan-tao-backend-cc9d64416e53.herokuapp.com/api/FoodType/', api_headers)
+                let data = await fetch('https://xiao-suan-tao-backend-cc9d64416e53.herokuapp.com/api/FoodType/', api_headers)
                 //let data = await fetch('http://127.0.0.1:8000/api/FoodType/',api_headers)
                 .then((res) => {
 
